13d26e55ab16d49b672773be133356170811b1af
Hugo Fernández <hugoemprendiendo@gmail.com>
2026-01-03 18:35:25 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogTrigger is no

--- src/app/(dashboard)/clients/page.tsx
+++ src/app/(dashboard)/clients/page.tsx
@@ -13,6 +13,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import {
